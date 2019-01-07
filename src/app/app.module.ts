762d92fb8c12bd8943235936b7cf70e3507dcd2c
Edwin <edwinlee168@outlook.com>
2019-01-07 11:30:12 +1100

    add material and flexlayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, FlexLayoutModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 import { HomePage } from './home.page';
 
@@ -11,6 +12,8 @@ import { HomePage } from './home.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    MatButtonModule,
+    MatIconModule,
     RouterModule.forChild([
       {
         path: '',
